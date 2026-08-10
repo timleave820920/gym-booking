e1452bb3db21b9fcc8ab957a034c578c5e59dca3
timleave820920 <19054591@qq.com>
2026-08-10 17:02:12 +0800

    feat: TabBar 文案调整（首页→今日，课程→本周）

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -2,8 +2,8 @@ Component({
   data: {
     selected: 0,
     list: [
-      { pagePath: '/pages/student-home/index', text: '首页', icon: 'home', selectedIcon: 'home' },
-      { pagePath: '/pages/student-courses/index', text: '课程', icon: 'grid', selectedIcon: 'grid' },
+      { pagePath: '/pages/student-home/index', text: '今日', icon: 'home', selectedIcon: 'home' },
+      { pagePath: '/pages/student-courses/index', text: '本周', icon: 'grid', selectedIcon: 'grid' },
       { pagePath: '/pages/student-my-courses/index', text: '我的课', icon: 'check', selectedIcon: 'check' },
       { pagePath: '/pages/student-profile/index', text: '我的', icon: 'user', selectedIcon: 'user' }
     ]
